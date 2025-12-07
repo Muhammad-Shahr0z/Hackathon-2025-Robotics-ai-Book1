0007ff34a624842809d72b07a1ca506222a27b70
Muhammad Shahroz <159715083+Muhammad-Shahr0z@users.noreply.github.com>
2025-12-08 00:47:41 +0500

    Update Root.tsx theme changes

--- robotics-textbook/book/src/theme/Root.tsx
+++ robotics-textbook/book/src/theme/Root.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { AuthProvider } from '../contexts/AuthContext';
 import AuthModal from '../components/AuthModal';
-import ChatWidget from '../components/ChatWidget/ChatWidget';
+// import ChatWidget from '../components/ChatWidget/ChatWidget';
 
 export default function Root({ children }) {
   return (
     <AuthProvider>
       {children}
       <AuthModal />
-      <ChatWidget />
+      {/* <ChatWidget /> */}
     </AuthProvider>
   );
 }
